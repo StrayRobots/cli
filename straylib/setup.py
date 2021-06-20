ecdc64da8f4cfa80a141a00574350d0f3d37ebda
Kenneth Blomqvist <kekeblom@gmail.com>
2021-06-20 19:45:54 +0200

    Stray lib

---
+++ straylib/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="straylib",
+    version="0.0.1",
+    author="Stray Robots",
+    author_email="ken@strayrobots.io",
+    description="Stray main utility library",
+    url="https://strayrobots.io",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    packages=['straylib'],
+    python_requires=">=3.6",
+)
