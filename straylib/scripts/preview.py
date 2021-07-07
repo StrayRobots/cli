c9c9a263b9e7f0d76171f470745c5bf036d79fef
Kenneth Blomqvist <kekeblom@gmail.com>
2021-07-07 13:03:46 +0200

    Fix preview script, allow pausing

--- straylib/scripts/preview.py
+++ straylib/scripts/preview.py
@@ -10,12 +10,12 @@ def read_args():
     parser = argparse.ArgumentParser(description="Convert Stray Scene datasets into a detectron2 dataset")
     parser.add_argument('scenes_folder', help="The directory where you keep your scenes.")
     parser.add_argument('--label', type=str, default="bbox_2d", help="The type of label to visualize.")
-    parser.add_argument('--rate', '-r', type=float, help="Frames per second to show frames.")
+    parser.add_argument('--rate', '-r', default=30.0, type=float, help="Frames per second to show frames.")
     return parser.parse_args()
 
 def main():
     flags = read_args()
-    examples = detectron2_dataset_function(flags.scenes_folder)
+    examples = detectron2_dataset_function(flags.scenes_folder)()
 
     if flags.label != 'bbox_2d':
         raise NotImplementedError(f"Unknown label type {flags.label}.")
@@ -23,8 +23,23 @@ def main():
     title = "Bounding Boxes"
     window = cv2.namedWindow(title, cv2.WINDOW_AUTOSIZE)
     cv2.setWindowProperty(title, cv2.WND_PROP_TOPMOST, 1)
+
     print("Playing through images.", end="\r")
+    paused = False
+
+    wait_time = int(1000.0 / flags.rate)
     for example in examples:
+        key = cv2.waitKey(wait_time)
+        if key == ord('q'):
+            break
+        elif key == ord(' '):
+            paused = not paused
+
+        while paused:
+            key = cv2.waitKey(wait_time)
+            if key == ord(' '):
+                paused = not paused
+
         filename = os.path.basename(example["file_name"])
         print(f"Image {filename}" + " " * 10, end='\r')
         image = cv2.imread(example["file_name"])
@@ -34,9 +49,6 @@ def main():
             lower_right = bbox[1]
             cv2.rectangle(image, tuple(upper_left - 3), tuple(lower_right + 3), (130, 130, 235), 3)
         cv2.imshow(title, image)
-        key = cv2.waitKey(int(1000.0 / flags.rate))
-        if key == ord('q'):
-            break
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
