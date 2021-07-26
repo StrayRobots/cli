2b41bf9c1d092ba9467393a1594a5ee5b27bca1d
Julius Hietala <julius.hietala@gmail.com>
2021-07-26 18:59:02 +0300

    dist setup

--- model/setup.py
+++ model/setup.py
@@ -12,6 +12,9 @@ setup(
     ],
     packages=find_packages(),
     python_requires=">=3.6",
+    install_requires=["click", "tensorboard", "cmake", "fvcore", "torchvision", "torch", "trimesh", "pyrender", "opencv-python"],
+    setup_requires=["fvcore", "torch==1.9", "torchvision==0.10"],
+    dependency_links=["https://download.pytorch.org/whl/cu111/torch_stable.html", "git+git://github.com/facebookresearch/fvcore.git@2d073f1b713f6ff3bb310af9a4313a5e8e03f49c"],
     entry_points={
         'console_scripts': [
             'stray-model-bake=straymodel.verbs.bake:bake',
