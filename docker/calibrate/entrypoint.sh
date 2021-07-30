96489cd7904acfd0ded4857c160d0739f907976c
Julius Hietala <julius.hietala@gmail.com>
2021-07-30 17:23:28 +0300

    mv camchain-bag.yaml

--- docker/calibrate/entrypoint.sh
+++ docker/calibrate/entrypoint.sh
@@ -18,6 +18,7 @@ then
 
   kalibr_bagcreater --folder /root/workspace/data --out /root/workspace/data/bag.bag
   kalibr_calibrate_cameras --bag /root/workspace/data/bag.bag --target /root/workspace/target.yaml --topics /cam0/image_raw /cam0/image_raw --models pinhole-equi pinhole-equi
+  mv camchain-rootworkspacedatabag.yaml camchain-bag.yaml
   python /root/workspace/extract_calibration.py --calibration camchain-bag.yaml --out /root/data/camera_intrinsics.json
 
   rm -rf /root/workspace/data
