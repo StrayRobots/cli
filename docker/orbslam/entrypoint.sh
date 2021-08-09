5e4388050e76eba64b83a9f54de48393dda80660
Kenneth Blomqvist <kekeblom@gmail.com>
2021-08-09 19:24:38 +0200

    Integrate scene point cloud

--- docker/orbslam/entrypoint.sh
+++ docker/orbslam/entrypoint.sh
@@ -7,23 +7,27 @@ set -e
 for d in "/home/user/data/"*; do
     if [ -d $d ]
         then
-        if [ -d "$d/scene" ] 
+        if [ -d "$d/scene" ]
         then
-            echo "Directory $d/scene exists, skipping." 
+            echo "Directory $d/scene exists, skipping."
         else
             echo "Computing trajectory for scene $d"
 
             python3 /home/user/workspace/make_settings.py $d --default-settings /home/user/workspace/default_settings.yaml -o /home/user/workspace/settings.yaml
 
-            pushd /home/user/orbslam/Examples/RGB-D
+            pushd /home/user/orbslam/Examples/RGB-D > /dev/null
 
             ./o3d ../../Vocabulary/ORBvoc.txt /home/user/workspace/settings.yaml $d
 
-            echo "Integrating."
+            echo "Integrating scene."
 
             python3 /home/user/workspace/integrate.py $d --trajectory CameraTrajectory.txt
 
-            popd
+            echo "Integrating point cloud."
+
+            python3 /home/user/workspace/integrate_pointcloud.py $d
+
+            popd > /dev/null
         fi
     fi
 done
