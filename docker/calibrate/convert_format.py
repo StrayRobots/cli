71536d8e7722f314e047c5951dd0b36841d3edb6
Kenneth Blomqvist <kekeblom@gmail.com>
2021-09-01 17:25:53 +0200

    Camera-imu calibration

--- docker/calibrate/convert_format.py
+++ docker/calibrate/convert_format.py
@@ -1,56 +1,90 @@
+from __future__ import print_function
 import argparse
 import os
 import csv
 from time import time
 from PIL import Image
 
-def main():
+DT = 1.0 / 60.0
+
+def read_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('scene')
     parser.add_argument('--out')
-    flags = parser.parse_args()
+    return parser.parse_args()
 
-    color_path = os.path.join(flags.scene, 'color')
-    imu_path = os.path.join(flags.out, 'imu0.csv')
-
-    color_images = os.listdir(color_path)
-    color_images = [i for i in color_images if 'jpg' in i]
-    color_images.sort()
-
-    dt = 1.0 / 60.0
-
-    os.makedirs(os.path.join(flags.out, 'cam0'))
-
-    start = time() * 1e9
-    print("Creating dataset.")
-    imu_readings = []
+def read_imu(flags):
+    out = []
     imu_in_path = os.path.join(flags.scene, 'imu.csv')
-    if os.path.exists():
+    if os.path.exists(imu_in_path):
         with open(imu_in_path, 'rt') as f:
             reader = csv.reader(f)
             next(reader)
             for row in reader:
-                imu_readings.append(row)
+                out.append(row)
+    return out
 
-    with open(imu_path, 'wt') as f:
+def move_imu(flags, color_image):
+    imu_path_out = os.path.join(flags.out, 'imu0.csv')
+    imu_readings = read_imu(flags)
+    with open(imu_path_out, 'wt') as f:
         writer = csv.writer(f)
 
         writer.writerow(['timestamp', 'omega_x', 'omega_y', 'omega_z', 'alpha_x','alpha_y', 'alpha_z'])
         if len(imu_readings) > 0:
             for reading in imu_readings:
                 # bagcreater uses ts, rotation and then acceleration
-                writer.writerow([reading[0], reading[4], reading[5], reading[6], reading[1], reading[2], reading[3]])
+                ts = str(int(float(reading[0]) * 1e9))
+                writer.writerow([ts, reading[4], reading[5], reading[6], reading[1], reading[2], reading[3]])
         else:
             for color_image in color_images:
                 num = os.path.basename(color_image).split('.')[0]
-                ts = str(int(start + float(num) * dt * 1e9))
+                ts = str(int(start + float(num) * DT * 1e9))
                 writer.writerow([ts] + [0 for _ in range(6)])
 
+def read_frames(flags, color_images):
+    frames_path = os.path.join(flags.scene, 'frames.csv')
+    timing = {}
+    if not os.path.exists(frames_path):
         for color_image in color_images:
-            in_path = os.path.join(flags.scene, 'color', color_image)
-            out_path = os.path.join(flags.out, 'cam0', ts + ".png")
-            Image.open(in_path).save(out_path)
-            print "Preprocessing image " + num
+            num = os.path.basename(color_image).split('.')[0]
+            ts = int(start + float(num) * DT * 1e9)
+            timing[num] = ts
+    else:
+        with open(frames_path, 'rt') as f:
+            reader = csv.reader(f)
+            next(reader) # header
+            for line in reader:
+                timestamp = line[0]
+                frame = line[1].strip()
+                timing[frame] = str(int(float(timestamp) * 1e9))
+    return timing
+
+def main():
+    flags = read_args()
+    color_path = os.path.join(flags.scene, 'color')
+
+    color_images = os.listdir(color_path)
+    color_images = [i for i in color_images if 'jpg' in i]
+    color_images.sort()
+
+    try:
+        os.makedirs(os.path.join(flags.out, 'cam0'))
+    except OSError:
+        pass
+
+    start = time() * 1e9
+    print("Creating dataset.")
+    move_imu(flags, color_images)
+    timing = read_frames(flags, color_images)
+
+    for color_image in color_images:
+        num = os.path.basename(color_image).split('.')[0]
+        ts = timing[num]
+        in_path = os.path.join(flags.scene, 'color', color_image)
+        out_path = os.path.join(flags.out, 'cam0', ts + ".png")
+        Image.open(in_path).save(out_path)
+        print("Preprocessing image " + num)
 
 if __name__ == "__main__":
     main()
--- docker/calibrate/entrypoint.sh
+++ docker/calibrate/entrypoint.sh
@@ -17,6 +17,7 @@ calibrate_cameras() {
   kalibr_calibrate_cameras --bag /root/workspace/data/bag.bag --target /root/workspace/target.yaml --topics /cam0/image_raw --models pinhole-equi
 
   mv camchain-rootworkspacedatabag.yaml camchain-bag.yaml
+  cp camchain-bag.yaml /root/data/camchain.yaml
 
   python /root/workspace/extract_calibration.py --calibration camchain-bag.yaml --out /root/data/camera_intrinsics.json
 
@@ -28,7 +29,37 @@ then
 
   mkdir -p /root/workspace/data
 
-  calibrate_cameras
+  args=("$@")
+  # Defaults to camera intrinsics calibration
+  # if --imu-noise is specified, it will assume the imu.csv file contains at least an hour
+  # of imu data where the device was static and not moving.
+  # --camera-imu is for calibrating camera to imu.
+  imu_noise_calibration=false
+  camera_imu_calibration=false
+  echo "Calibration run $@"
+  for arg in "$@"
+  do
+    if [ "${args[i]}" = "--imu-noise" ]
+    then
+      imu_noise_calibration=true
+    elif [ "${args[i]}" = "--camera-imu" ]
+    then
+      camera_imu_calibration=true
+    fi
+    i=$((i + 1))
+  done
+
+  if [ "$imu_noise_calibration" = true ]
+  then
+    echo "Calibrating imu noise"
+    /root/workspace/imu/run.sh $@
+  elif [ "$camera_imu_calibration" = true ]
+  then
+    echo "Calibrating camera to imu"
+    /root/workspace/camera_imu/run.sh $@
+  else
+    calibrate_cameras $@
+  fi
 
   rm -rf /root/workspace/data
 elif [ "$1" = "generate" ]
