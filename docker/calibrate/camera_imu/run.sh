91d77f41ae8b4cf0e80edd2e8a5f2ffeeb5ba093
Kenneth Blomqvist <kekeblom@gmail.com>
2021-09-06 13:10:26 +0200

    Fix camera_imu calibration, don't require rostopic in imu calibration yaml

--- docker/calibrate/camera_imu/run.sh
+++ docker/calibrate/camera_imu/run.sh
@@ -2,12 +2,11 @@
 set -e
 
 python /root/workspace/convert_format.py /root/data/ --out /tmp/data
+python /root/workspace/camera_imu/create_imu_config.py /root/workspace/imu.yaml --out /tmp/imu.yaml
 
 kalibr_bagcreater --folder /tmp/data --out /tmp/data/bag.bag
 
-kalibr_calibrate_imu_camera --bag /tmp/data/bag.bag --target /root/workspace/target.yaml --cam /root/workspace/camera.yaml --imu /root/workspace/imu.yaml --dont-show-report
-
-/bin/bash
+kalibr_calibrate_imu_camera --bag /tmp/data/bag.bag --target /root/workspace/target.yaml --cam /root/workspace/camera.yaml --imu /tmp/imu.yaml --dont-show-report
 
 mv camchain-imucam-tmpdatabag.yaml /root/data/camchain-imucam.yaml
 mv results-imucam-tmpdatabag.txt /root/data/results-imucam.txt
