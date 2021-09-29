69670e72b73079ccd998b56359acbfad54887036
Kenneth Blomqvist <kekeblom@gmail.com>
2021-09-29 21:39:57 +0200

    Bounding box loss

--- model/straymodel/loss.py
+++ model/straymodel/loss.py
@@ -2,6 +2,12 @@ import torch
 from torch.nn.modules.loss import _Loss
 import torch.nn.functional as F
 
+def spatial_softmax(predictions):
+    """
+    Convert predictions to heatmaps by normalizing over the 2d image.
+    """
+    N, H, W = predictions.shape
+    return F.softmax(predictions.reshape(N, -1), dim=1).reshape(N, H, W)
 
 class IntegralKeypointLoss(_Loss):
     def __init__(self, size):
@@ -28,13 +34,24 @@ class IntegralKeypointLoss(_Loss):
         xyz = torch.stack([x, y, torch.ones_like(x)], dim=-1)
         return xyz * depth[:, None]
 
-    def _heatmap(self, x):
-        # x: N x H x W
-        N, H, W = x.shape
-        return F.softmax(x.reshape(N, -1), dim=1).reshape(N, H, W)
-
-    def forward(self, prediction, depthmap, K, target):
-        heatmap = self._heatmap(prediction)
+    def forward(self, heatmap, depthmap, K, target):
         points3d = self._integrate_maps_unproject_points(heatmap, depthmap, K)
         return F.l1_loss(points3d, target)
+
+class BoundingBoxLoss(_Loss):
+    def __init__(self, size, center_weight=1.0, heatmap_weight=1.0, corner_weight=1.0):
+        super().__init__()
+        self.integral_loss = IntegralKeypointLoss(size)
+        self.center_weight = center_weight
+        self.heatmap_weight = heatmap_weight
+        self.corner_weight = corner_weight
+
+    def forward(self, p_heatmap, p_depth, p_corners, gt_heatmap, gt_corners, Ks, gt_points):
+        center_loss = self.integral_loss(p_heatmap, p_depth, Ks, gt_points)
+        l2_heatmap_loss = F.mse_loss(p_heatmap, gt_heatmap)
+        corner_loss = F.l1_loss(p_corners, gt_corners)
+        return (self.center_weight * center_loss +
+                self.heatmap_weight * l2_heatmap_loss +
+                self.corner_weight * corner_loss)
+
 
--- model/test/test_loss.py
+++ model/test/test_loss.py
@@ -1,3 +1,4 @@
+import itertools
 import unittest
 import torch
 import cv2
@@ -5,6 +6,7 @@ import numpy as np
 from torch import optim
 from straymodel.loss import *
 import torch.nn.functional as F
+from scipy.spatial.transform import Rotation
 
 WIDTH = 256
 HEIGHT = 256
@@ -61,12 +63,12 @@ class LossTestCase(unittest.TestCase):
         loss = IntegralKeypointLoss((HEIGHT, WIDTH))
         for _ in range(250):
             optimizer.zero_grad()
-            value = loss(x_heatmap, x_depth, K_tensor, keypoint)
+            value = loss(spatial_softmax(x_heatmap), x_depth, K_tensor, keypoint)
             value.backward()
             optimizer.step()
             schedule.step()
 
-        points3d = loss._integrate_maps_unproject_points(loss._heatmap(x_heatmap), x_depth, torch.tensor(K)).detach().numpy()
+        points3d = loss._integrate_maps_unproject_points(spatial_softmax(x_heatmap), x_depth, torch.tensor(K)).detach().numpy()
         np.testing.assert_allclose(points3d[0], keypoint[0], rtol=1e-2, atol=1e-2)
 
     def test_optimize_batch(self):
@@ -87,18 +89,60 @@ class LossTestCase(unittest.TestCase):
         loss = IntegralKeypointLoss((HEIGHT, WIDTH))
         for _ in range(250):
             optimizer.zero_grad()
-            value = loss(x_heatmap, x_depth, K_tensor, keypoints)
+            value = loss(spatial_softmax(x_heatmap), x_depth, K_tensor, keypoints)
             value.backward()
             optimizer.step()
             schedule.step()
 
-        points3d = loss._integrate_maps_unproject_points(loss._heatmap(x_heatmap), x_depth, torch.tensor(K)).detach().numpy()
+        points3d = loss._integrate_maps_unproject_points(spatial_softmax(x_heatmap), x_depth, torch.tensor(K)).detach().numpy()
         np.testing.assert_allclose(points3d, keypoints.numpy(), rtol=1e-2, atol=1e-2)
 
+    def test_full_loss(self):
+        center = np.random.uniform(np.ones(3) * -0.1, np.ones(3) * 0.1) + np.array([0., 0., 1.])
+        bbox = np.array([-1., -1., -1.,
+            -1., -1.,  1.,
+            -1.,  1., -1.,
+            -1.,  1.,  1.,
+             1., -1., -1.,
+             1., -1.,  1.,
+             1.,  1., -1.,
+             1.,  1.,  1.]).reshape((8, 3)) * 0.1
+        R = Rotation.random()
+        bbox = R.apply(bbox)
+        bbox += center
+        points2d, _ = cv2.projectPoints(bbox[:, None, :], np.zeros(3), np.zeros(3), K, D)
+        points2d = points2d.reshape(8, 2)
+        center2d, _ = cv2.projectPoints(center[None, None, :], np.zeros(3), np.zeros(3), K, D)
+        center2d = center2d.ravel()
+        heatmap = np.zeros((1, HEIGHT, WIDTH))
+        _paint_heatmap(heatmap[0], center2d[None])
+        corners = np.zeros((1, 16, HEIGHT, WIDTH))
+        for i, point in enumerate(points2d):
+            vector = center2d - point
+            vector_field = vector[:, None, None].repeat(HEIGHT, axis=1).repeat(WIDTH, axis=2)
+            index = i*2
+            corners[:, index:index+2] = vector_field
+        loss = BoundingBoxLoss((HEIGHT, WIDTH))
+        depth = np.ones((1, HEIGHT, WIDTH)) * center[2]
+        heatmap, depth, corners, center = torch.tensor(heatmap), torch.tensor(depth), torch.tensor(corners), torch.tensor(center)
+        K_tensor = torch.tensor(K)
+        heatmap /= heatmap.sum()
+        value = loss(heatmap, depth, corners, heatmap, corners, K_tensor, center[None])
+        self.assertAlmostEqual(value.item(), 0.0)
 
-
-
-
+        p_heatmap = (heatmap.clone() * 100.).requires_grad_(True)
+        p_depth = depth.clone().requires_grad_(True)
+        p_corners = corners.clone().requires_grad_(True)
+        optimizer = optim.SGD([p_heatmap, p_depth, p_corners], lr=1000., momentum=0.9)
+        for _ in range(100):
+            optimizer.zero_grad()
+            value = loss(spatial_softmax(p_heatmap), p_depth, p_corners, heatmap, corners, K_tensor, center[None])
+            value.backward()
+            optimizer.step()
+        self.assertLess(value.item(), 1e-2)
+        np.testing.assert_allclose(spatial_softmax(p_heatmap).detach().numpy(), heatmap.numpy(), rtol=0.1, atol=1e-2)
+        np.testing.assert_allclose(p_corners.detach().numpy(), corners.numpy())
+        np.testing.assert_allclose(p_depth.detach().numpy(), depth.numpy(), rtol=0.1, atol=1e-3)
 
 
 if __name__ == "__main__":
