5e28327f55b53ddff16e8456d110ef34f159629f
Julius Hietala <julius.hietala@gmail.com>
2021-10-15 12:53:31 +0300

    inference

--- model/straymodel/train/inference.py
+++ model/straymodel/train/inference.py
@@ -3,36 +3,35 @@ import click
 from torch.utils.data import DataLoader
 import torch
 from straymodel.utils.visualization_utils import save_example
+from straymodel.train.loss import spatial_softmax
+import os
 
 
-def inference(model, scenes, inference_save_path, num_examples, device="cpu"):
+def inference(scene, model, device="cpu"):
     model.to(device)
     model.eval()
-    dataset = Stray3DBoundingBoxDetectionDataset(scenes, out_size=(80, 60))
-    inference_loader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=1)
+    dataset = Stray3DBoundingBoxDetectionDataset([scene], out_size=(80, 60))
+    inference_loader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=1)
+    inference_save_path = os.path.join(scene, "3d_labeled_examples")
+    os.makedirs(inference_save_path, exist_ok=True)
     for i, batch in enumerate(inference_loader):
         images, _, _, cameras, _, sizes = [item.to(device) for item in batch]
 
         p_heatmaps, _, p_corners = model(images)
-    
-        if i >= num_examples:
-            break
 
-        save_example(images[0].cpu().numpy(), p_heatmaps[0].detach().cpu().numpy(), p_corners[0].detach().cpu().numpy(), cameras[0].cpu().numpy(), sizes[0].cpu().numpy(), inference_save_path, i)
+        save_example(images[0].cpu().numpy(), spatial_softmax(p_heatmaps)[0].detach().cpu().numpy(), p_corners[0].detach().cpu().numpy(), cameras[0].cpu().numpy(), sizes[0].cpu().numpy(), inference_save_path, i)
 
 
 @click.command()
 @click.argument('scene', nargs=1)
 @click.option('--model-path', required=True, type=str)
-@click.option('--inference-save-path', default="./", type=str)
-@click.option('--num-examples', default=100, type=int)
-def main(scenes, model_path, inference_save_path, num_examples):
+def main(scene, model_path):
     if torch.cuda.is_available():
         device = "cuda:0"
     else:
         device = "cpu"
     model = torch.jit.load(model_path)
-    inference(scenes, model, inference_save_path, num_examples, device)
+    inference(scene, model, device)
 
 
 
