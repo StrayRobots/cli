72424af345d383a2edd3caa8278ed7c592708f35
Kenneth Blomqvist <ken@strayrobots.io>
2021-10-24 14:59:54 +0200

    Parallelize dataloading, remove tf dependency

--- model/straymodel/train/objectron/train_objectron.py
+++ model/straymodel/train/objectron/train_objectron.py
@@ -4,10 +4,16 @@ import torch.optim as optim
 import torch
 from straymodel.train.objectron.objectron_loss import BoundingBoxLoss, spatial_softmax
 import os
-import tensorflow as tf
 from straymodel.train.objectron.utils import *
+from tfrecord.torch.dataset import TFRecordDataset
 
-
+def create_dataset(files):
+    blank_corner_map, blank_heatmap = get_blank_maps()
+    return (TFRecordDataset(files[0], None)
+            .map(unpack_record(blank_corner_map, blank_heatmap))
+            .filter(lambda xy: xy[0])
+            .map(lambda xy: xy[1])
+            )
 
 @click.command()
 @click.argument('tfdata', nargs=1)
@@ -15,13 +21,11 @@ from straymodel.train.objectron.utils import *
 @click.option('--progress-save-folder', default="./")
 @click.option('--num-workers', type=int, default=1)
 @click.option('--num-epochs', type=int, default=1)
-@click.option('--heatmap-loss-coef', type=int, default=1)
-@click.option('--corner-loss-coef', type=int, default=1)
+@click.option('--heatmap-loss-coef', type=float, default=1.0)
+@click.option('--corner-loss-coef', type=float, default=1.0)
 def train(tfdata, batch_size, progress_save_folder, num_workers, num_epochs, heatmap_loss_coef, corner_loss_coef):
 
     os.makedirs(progress_save_folder, exist_ok=True)
-    #Disable GPU from tf to avoid OOM
-    tf.config.set_visible_devices([], 'GPU')
 
     model = StrayNet()
 
@@ -34,43 +38,42 @@ def train(tfdata, batch_size, progress_save_folder, num_workers, num_epochs, hea
 
     loss_function = BoundingBoxLoss(heatmap_loss_coef, corner_loss_coef)
     optimizer = optim.Adam(model.parameters())
-    blank_corner_map, blank_heatmap = get_blank_maps()
 
+    files = [os.path.join(tfdata, f) for f in os.listdir(tfdata)]
     for epoch in range(num_epochs):
-        for i, file in enumerate(os.listdir(tfdata)):
-            dataset = tf.data.TFRecordDataset(os.path.join(tfdata,file), num_parallel_reads=num_workers)
-            dataset = dataset.batch(batch_size)
-            print(f"Epoch {epoch}, TFRecord file {i} ({file})")
-            for j, tf_batch in enumerate(dataset.take_while(lambda x: True)):
-                valid_batch, batch = get_batch_from_record(tf_batch, blank_heatmap, blank_corner_map)
+        print(f"Epoch {epoch}")
+        dataset = create_dataset(files)
+        dataloader = torch.utils.data.DataLoader(dataset,
+                batch_size=batch_size,
+                num_workers=num_workers,
+                pin_memory=True)
+        for i, batch in enumerate(dataloader):
+            print(f"Batch {i}", end='\r')
+            optimizer.zero_grad()
 
-                if not valid_batch:
-                    continue
+            images, heatmaps, corner_maps, intrinsics, sizes = batch
 
-                images, heatmaps, corner_maps, intrinsics, sizes = batch
+            if i == 0 and epoch == 0:
+                sample_folder = os.path.join(progress_save_folder, "sample")
+                os.makedirs(sample_folder, exist_ok=True)
+                save_objectron_sample(sample_folder, images.numpy(), heatmaps.numpy(), corner_maps.numpy(), intrinsics, sizes)
 
-                if i == 0 and j == 0 and epoch == 0:
-                    sample_folder = os.path.join(progress_save_folder, "sample")
-                    os.makedirs(sample_folder, exist_ok=True)
-                    save_objectron_sample(sample_folder, images.numpy(), heatmaps.numpy(), corner_maps.numpy(), intrinsics, sizes)
-                    
-                optimizer.zero_grad()
-                images = images.to(device)
-                heatmaps = heatmaps.to(device)
-                corner_maps = corner_maps.to(device)
-                p_heatmaps, _, p_corners = model(images)
+            images = images.to(device)
+            heatmaps = heatmaps.to(device)
+            corner_maps = corner_maps.to(device)
+            p_heatmaps, _, p_corners = model(images)
 
-                if i == 0 and j == 0:
-                    epoch_folder = os.path.join(progress_save_folder, f"epoch_{epoch}")
-                    os.makedirs(epoch_folder, exist_ok=True)
-                    save_objectron_sample(epoch_folder, images.cpu().numpy(), spatial_softmax(p_heatmaps).detach().cpu().numpy(), p_corners.detach().cpu().numpy(), intrinsics, sizes)
-                    #Don't train on this single batch
-                    continue
-        
-                heatmap_loss, corner_loss = loss_function(p_heatmaps, p_corners, heatmaps, corner_maps)
-                loss = heatmap_loss + corner_loss
-                loss.backward()
-                optimizer.step()
+            if i == 0:
+                epoch_folder = os.path.join(progress_save_folder, f"epoch_{epoch}")
+                os.makedirs(epoch_folder, exist_ok=True)
+                save_objectron_sample(epoch_folder, images.cpu().numpy(), spatial_softmax(p_heatmaps).detach().cpu().numpy(), p_corners.detach().cpu().numpy(), intrinsics, sizes)
+                #Don't train on this single batch
+                continue
+
+            heatmap_loss, corner_loss = loss_function(p_heatmaps, p_corners, heatmaps, corner_maps)
+            loss = heatmap_loss + corner_loss
+            loss.backward()
+            optimizer.step()
 
         #Saves model every epoch
         script_model = torch.jit.script(model)
--- model/straymodel/train/objectron/utils.py
+++ model/straymodel/train/objectron/utils.py
@@ -2,13 +2,10 @@ import torch
 from straymodel.utils.visualization_utils import save_example
 import os
 from straymodel.train.objectron import objectron_features as features
-import tensorflow as tf
 import numpy as np
 from straymodel.utils.heatmap_utils import paint_heatmap
 import cv2
 from straylib.camera import get_scaled_camera_matrix
-import tensorflow as tf
-
 
 I = np.eye(3)
 WIDTH = 480
@@ -27,16 +24,10 @@ def get_blank_maps():
     blank_heatmap = np.zeros((1, OUT_HEIGHT, OUT_WIDTH), dtype=np.float32)
     return blank_corner_map, blank_heatmap
 
-def get_tfdata_field(data, feature_name):
-    return data[features.FEATURE_NAMES[feature_name]].values.numpy()
-
-def get_tfdata_image(data):
-    data['image'] = tf.image.decode_png(data[features.FEATURE_NAMES['IMAGE_ENCODED']], channels=NUM_CHANNELS)
-    data['image'].set_shape([HEIGHT, WIDTH, NUM_CHANNELS])
-    data['image'] = tf.cast(data['image'], tf.float32) * (2. / 255.) - 1.0
-    image = tf.cast((data['image'] + 1.0) / 2.0 * 255, tf.uint8).numpy()
-    image = np.moveaxis(image, -1, 0)
-    return image[np.newaxis, ...]
+def get_image(data):
+    data['image'] = cv2.imdecode(data['image/encoded'], -1)
+    data['image'] = data['image'].astype(np.float32) / 255.0
+    return np.transpose(data['image'], [2, 0, 1])
 
 def get_image_filename(data):
     return data[features.FEATURE_NAMES['IMAGE_FILENAME']].numpy().decode("utf-8")
@@ -45,10 +36,10 @@ def get_image_id(data):
     return data[features.FEATURE_NAMES['IMAGE_ID']].numpy()[0]
 
 def get_heatmap(data, blank_heatmap):
-    translation = get_tfdata_field(data, 'OBJECT_TRANSLATION')
-    intrinsics = get_tfdata_field(data, 'INTRINSIC_MATRIX').reshape((3,3))
-    scale = get_tfdata_field(data, 'OBJECT_SCALE')
-    points_2d = get_tfdata_field(data, 'POINT_2D')
+    translation = data['object/translation']
+    intrinsics = data['camera/intrinsics'].reshape((3, 3))
+    scale = data['object/scale']
+    points_2d = data['point_2d']
 
     scaled_intrinsics = get_scaled_camera_matrix(intrinsics, 0.125, 0.125)
     heatmap = np.copy(blank_heatmap)
@@ -62,43 +53,32 @@ def get_heatmap(data, blank_heatmap):
     lengthscale = np.sqrt(size**2/20.0)
     center_point = points_2d[:3][:2]*np.array([OUT_WIDTH, OUT_HEIGHT])
     paint_heatmap(heatmap[0], [center_point], lengthscale)
-    return heatmap[np.newaxis, ...]
+    return heatmap
 
 def get_corner_maps(data, blank_corner_map):
     corner_map = np.copy(blank_corner_map)
-    points_2d = get_tfdata_field(data, 'POINT_2D')
+    points_2d = data['point_2d']
     projected = points_2d[3:].reshape((8,3))[:,:2]*np.array([OUT_WIDTH, OUT_HEIGHT])
     for j, point in enumerate(projected):
         corner_map[j*2] = point[0] - corner_map[j*2]
         corner_map[j*2 +1] = point[1] - corner_map[j*2+1]
-    return corner_map[np.newaxis, ...]
+    return corner_map
 
 def save_objectron_sample(folder, images, heatmaps, corner_maps, cameras, sizes):
     for j, (image, heatmap, corner_map, camera, size) in enumerate(zip(images, heatmaps, corner_maps, cameras, sizes)):
         save_example(image, heatmap, corner_map, camera, size, folder, j)
 
-def get_batch_from_record(tf_batch, blank_heatmap, blank_corner_map):
-    valid_batch = False
-    for raw_record in tf_batch:
-        data = tf.io.parse_single_example(raw_record, features = features.FEATURE_MAP)
-        instances = data[features.FEATURE_NAMES['INSTANCE_NUM']].numpy()
+def unpack_record(blank_corner_map, blank_heatmap):
+    def inner(data):
+        instances = data['instance_num']
         if not instances[0] == 1:
-            continue
-        elif not valid_batch:
-            images = torch.from_numpy(get_tfdata_image(data)).float()
+            return False, None
+        else:
+            images = torch.from_numpy(get_image(data)).float()
             heatmaps = torch.from_numpy(get_heatmap(data, blank_heatmap)).float()
             corner_maps = torch.from_numpy(get_corner_maps(data, blank_corner_map)).float()
-            intrinsics = get_tfdata_field(data, 'INTRINSIC_MATRIX').reshape((1,3,3))
-            sizes = get_tfdata_field(data, 'OBJECT_SCALE')
-            valid_batch = True
-        else:
-            images = torch.cat((images, torch.from_numpy(get_tfdata_image(data)).float()))
-            heatmaps = torch.cat((heatmaps, torch.from_numpy(get_heatmap(data, blank_heatmap)).float()))
-            corner_maps = torch.cat((corner_maps, torch.from_numpy(get_corner_maps(data, blank_corner_map)).float()))
-            intrinsics = np.vstack((intrinsics, get_tfdata_field(data, 'INTRINSIC_MATRIX').reshape((1,3,3))))
-            sizes = np.vstack((sizes, get_tfdata_field(data, 'OBJECT_SCALE')))
-    
-    if valid_batch:
-        return valid_batch, (images, heatmaps, corner_maps, intrinsics, sizes)
-    else:
-        return valid_batch, None
+            intrinsics = data['camera/intrinsics'].reshape((3, 3))
+            sizes = data['object/scale']
+            return True, (images, heatmaps, corner_maps, intrinsics, sizes)
+    return inner
+
