85331c66eaefd4dcc667ccc69af0e52b6e14e786
Julius Hietala <julius.hietala@gmail.com>
2021-11-02 16:09:39 +0200

    mods

--- straymodel/straymodel/sdk/oriented_bounding_box_detector.py
+++ straymodel/straymodel/sdk/oriented_bounding_box_detector.py
@@ -6,44 +6,40 @@ import numpy as np
 import cv2
 from skspatial.objects import Plane, Line
 
-def load_config(model_path):
+def load_config(model_path, minimum_confidence):
     config = get_cfg()
     if os.path.isfile(os.path.join(model_path, "config.yaml")):
         config.merge_from_file(os.path.join(model_path, "config.yaml"))
     
     config.MODEL.WEIGHTS = os.path.join(model_path, "output", "model_final.pth")
     config.MODEL.DEVICE = 'cpu'
+    config.MODEL.ROI_HEADS.SCORE_THRESH_TEST = minimum_confidence
     return config
 
 
 class OrientedBoundingBoxDetector(object):
-    def __init__(self, *args, **kwargs):
-        self.cfg = kwargs.get('cfg', {})
+    def __init__(self, model, image_width, image_height, fx, fy, depth_scale, minimum_confidence=0.9, depth_box_scale=0.5):
+        self.cfg = load_config(model, minimum_confidence)
         self.predictor = DefaultPredictor(self.cfg)
+        self.image_width = image_width
+        self.image_height = image_height
+        self.fx = fx
+        self.fy = fy
+        self.depth_scale = depth_scale
+        self.depth_box_scale = depth_box_scale
 
-    @staticmethod
-    def from_directory(model_path):
-        cfg = load_config(model_path)
-        return OrientedBoundingBoxDetector(cfg=cfg)
-
-    @staticmethod
-    def from_cfg(cfg):
-        return OrientedBoundingBoxDetector(cfg=cfg)
-
-    @staticmethod
-    def get_ray(width, height, x, y, fx, fy):
-        aspect_ratio = width / height
-        fovx = 2*math.atan(width/(2*fx))
-        fovy= 2*math.atan(height/(2*fy))
-        pX = (2.0 * ((x + 0.5) / width) - 1.0) * math.tan(fovx / 2.0) * aspect_ratio
-        pY = (1.0 - 2.0 * ((y + 0.5) / height)) * math.tan(fovy / 2.0)
+    def get_ray(self, x, y):
+        aspect_ratio = self.image_width / self.image_height
+        fovx = 2*math.atan(self.image_width/(2*self.fx))
+        fovy= 2*math.atan(self.image_height/(2*self.fy))
+        pX = (2.0 * ((x + 0.5) / self.image_width) - 1.0) * math.tan(fovx / 2.0) * aspect_ratio
+        pY = (1.0 - 2.0 * ((y + 0.5) / self.image_height)) * math.tan(fovy / 2.0)
         vector = np.array([pX, -pY, 1])
         return vector / np.linalg.norm(vector)
 
-    @staticmethod
-    def get_point_3d_from_depth(instance, depth, image_width, image_height, depth_scale, fx, fy, scale=0.5):
-        mask = np.float32(np.zeros((image_height, image_width, 3)))
-        x, y, width, height, rotation = instance["x"], instance["y"], instance["width"]*scale, instance["height"]*scale, instance["rotation"]
+    def get_point_3d_from_depth(self, instance, depth):
+        mask = np.float32(np.zeros((self.image_height, self.image_width, 3)))
+        x, y, width, height, rotation = instance["x"], instance["y"], instance["width"]*self.depth_box_scale, instance["height"]*self.depth_box_scale, instance["rotation"]
         rect = ((x, y), (width, height), rotation)
         cv_box = cv2.boxPoints(rect)
         cv_box = np.int0(cv_box)
@@ -52,32 +48,41 @@ class OrientedBoundingBoxDetector(object):
         mask = cv2.threshold(mask, 100, 1, cv2.THRESH_BINARY)[1]
         mask = mask.astype(bool)
         masked_depth = depth[mask]
-        length = np.mean(masked_depth[np.nonzero(masked_depth)])*depth_scale
+        length = np.mean(masked_depth[np.nonzero(masked_depth)])*self.depth_scale
+        return self.get_ray(x, y)*length
 
-        return OrientedBoundingBoxDetector.get_ray(image_width, image_height, x, y, fx, fy)*length
-
-    @staticmethod
-    def get_point_3d_from_height(instance, camera_height, pick_height,  image_width, image_height, fx, fy):
+    def get_point_3d_from_height(self, instance, z):
         x, y = instance["x"], instance["y"]
-        plane_C = Plane(point=[0, 0, camera_height-pick_height], normal=[0, 0, -1])
-        ray = OrientedBoundingBoxDetector.get_ray(image_width, image_height, x, y, fx, fy)
+        plane_C = Plane(point=[0, 0, z], normal=[0, 0, -1])
+        ray = self.get_ray(x, y)
         line_C = Line([0, 0, 0], ray)
         point_3d_C = plane_C.intersect_line(line_C)
         return np.array(point_3d_C)
 
-    def __call__(self, color):
+    def __call__(self, color, depth, z=None):
         predictions = []
         raw_predictions = self.predictor(color)
         raw_bounding_boxes = raw_predictions["instances"].to("cpu").pred_boxes.tensor.numpy()
-        for i, raw_bounding_box in enumerate(raw_bounding_boxes):
+        categories = raw_predictions["instances"].to("cpu").pred_classes.numpy()
+        confidences = raw_predictions["instances"].to("cpu").scores.numpy()
+
+        for i, (bbox, category, confidence) in enumerate(zip(raw_bounding_boxes, categories, confidences)):
             instance = {
                 "instance_id": i,
-                "x": raw_bounding_box[0],
-                "y": raw_bounding_box[1],
-                "width" : raw_bounding_box[2],
-                "height": raw_bounding_box[3],
-                "rotation": -raw_bounding_box[4],
+                "category_id": category,
+                "confidence": confidence,
+                "x": bbox[0],
+                "y": bbox[1],
+                "width" : bbox[2],
+                "height": bbox[3],
+                "rotation": -bbox[4]
             }
+            if z is not None:
+                point_3d = self.get_point_3d_from_height(instance, z)
+            else:
+                point_3d = self.get_point_3d_from_depth(instance, depth)
+
+            instance["point_3d"] = point_3d
             predictions.append(instance)
 
         return predictions
