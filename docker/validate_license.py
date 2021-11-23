f3e423fb1ac40f90bbd75d53ed941857425553af
Julius Hietala <jh@Juliuss-MacBook-Pro.local>
2021-11-23 17:40:36 +0200

    edits

--- docker/validate_license.py
+++ docker/validate_license.py
@@ -3,22 +3,25 @@ import json
 import os
 
 def validate_license():
-  validation = requests.post(
-    "https://api.keygen.sh/v1/accounts/strayrobots-io/licenses/actions/validate-key",
-    headers={
-      "Content-Type": "application/vnd.api+json",
-      "Accept": "application/vnd.api+json"
-    },
-    data=json.dumps({
-      "meta": {
-        "key": os.environ['STRAY_ACTIVATION_KEY']
-      }
-    })
-  ).json()
+  validation = {}
+  try:
+    validation = requests.post(
+      "https://api.keygen.sh/v1/accounts/strayrobots-io/licenses/actions/validate-key",
+      headers={
+        "Content-Type": "application/vnd.api+json",
+        "Accept": "application/vnd.api+json"
+      },
+      data=json.dumps({
+        "meta": {
+          "key": os.environ['STRAY_LICENSE_KEY']
+        }
+      })
+    ).json()
+  finally:
+    if not validation.get("meta", {}).get("valid", False):
+      print("Could not validate license key, please contact hello@strayrobots.io for help.")
+      exit(1)
 
-  if not validation["meta"]["valid"]:
-    print("Invalid license, please contact hello@strayrobots.io for help.")
-    exit(1)
   
 
 if __name__ == "__main__":
